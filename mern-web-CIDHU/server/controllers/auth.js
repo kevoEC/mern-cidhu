723f6072686c2eee8feed80aa933ab9dc004cfff
Kevin Rosero <kevin.rosero@sginvierte.com>
2023-10-02 17:22:33 -0500

    creacion de usuarios backend

--- mern-web-CIDHU/server/controllers/auth.js
+++ mern-web-CIDHU/server/controllers/auth.js
@@ -1,5 +1,23 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
+
 function register(req, res){
-    console.log("Se ha ejecutado el controlador de registro de usuario")
+    const {firstname, lastname, email, password} = req.body;
+
+    console.log(req.body);
+    if(!email) res.status(400).send({message: "El email es obligatorio"});
+    if(!password) res.status(400).send({message: "La contraseña es obligatoria"});
+
+    const user = new User({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        password,
+        role: "user",
+        active: false
+    });
+    const salt = bcrypt.genSaltSync(10);
+    
     res.status(200).send({
         message: "TODO OK"
     });

3551741cc30e1932290ea646b598a72929ce1f65
Kevin Rosero <115098318+kevoEC@users.noreply.github.com>
2023-10-05 16:55:12 -0500

    Creacion Api de ingreso usuarios junto con la conexion a la base mediante un middleware que recibe un header

--- mern-web-CIDHU/server/controllers/user.js
+++ mern-web-CIDHU/server/controllers/user.js
@@ -1,7 +1,51 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
+
 async function getMe(req, res){
-    res.status(200).send({message: "OK"});
+    const {user_id} = req.user;
+    const response = await User.findById(user_id);
+
+    if(!response){
+        res.status(404).send({message: "Usuario no encontrado"});
+    } else {
+        res.status(200).send({user: response});
+    }
+}
+
+async function getUsers(req, res){
+    const {active} = req.query;
+    let response = null;
+
+    if(active == undefined){
+        response = await User.find();
+    }else{
+        response = await User.find({active});
+    }
+    res.status(200).send(response);
+}
+
+async function createUser(req, res){
+    const {password} = req.body;
+    const user = new User({...req.body, active: false});
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+    user.password = hashPassword;
+    if(req.files){
+        //TO DO: Procesar avatar
+        console.log("Procesar avatar");
+    }
+
+    user.save((error, userStored) => {
+        if(error){
+            res.status(500).send({message: `Error al crear el usuario: ${error}`});
+        }else{
+            res.status(201).send({userStored});
+        }
+    });
 }
 
 module.exports = {
     getMe,
+    getUsers,
+    createUser
 }
--- mern-web-CIDHU/server/middlewares/authenticated.js
+++ mern-web-CIDHU/server/middlewares/authenticated.js
@@ -1,8 +1,26 @@
 const jwt = require("../utils/jwt");
 
 function asureAuth(req, res, next) {
-    console.log("Me encuentro en ASURE AUTH");
-    next();
+    if (!req.headers.authorization) {
+        res
+        .status(403)
+        .send({ message: "La peticion no tiene la cabecera de autenticacion" });
+    }
+    const token = req.headers.authorization.replace("Bearer ", "");
+    try{
+        const payload = jwt.decodeToken(token);
+        const { exp } = payload;
+        const currentData = new Date().getTime();
+
+        if (currentData >= exp) {
+            return res.status(404).send({ message: "El token ha expirado" });
+        }
+
+        req.user = payload;
+        next();
+    }catch (error){
+        return res.status(404).send({message: "Token invalido"});
+    }
 }
 
 module.exports = {
